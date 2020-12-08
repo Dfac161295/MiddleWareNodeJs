cdc142d8a88ab0da9d296111508117fa100f9ca0
Daniel Fernando Argueta <dfargueta@gyt.com.gt>
2020-12-08 10:10:14 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+// parse application/json
+app.use(bodyParser.json());
+
+//Middleware function
+function isValid(req, res, next) {
+
+    if (req.body.isValid == true) {
+        console.log('true');
+        next();
+    } else {
+        console.log('false');
+        res.status(403).send('unauthorized');
+    }
+
+}
+
+app.use(isValid);
+
+
+app.get('/saludo', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
